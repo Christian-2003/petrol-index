1a705ff8b215a36e3ac923f9727b943de9229db9
Christian-2003 <80711177+Christian-2003@users.noreply.github.com>
2025-01-07 17:20:17 +0100

    Add database migration code

--- app/src/main/java/de/christian2003/petrolindex/database/PetrolIndexDatabase.kt
+++ app/src/main/java/de/christian2003/petrolindex/database/PetrolIndexDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 
 /**
@@ -12,7 +14,7 @@ import androidx.room.RoomDatabase
  * @author  Christian-2003
  * @since   1.0.0
  */
-@Database(entities = [PetrolEntry::class], version = 1, exportSchema = false)
+@Database(entities = [PetrolEntry::class], version = 2, exportSchema = false)
 abstract class PetrolIndexDatabase: RoomDatabase() {
 
     /**
@@ -30,6 +32,15 @@ abstract class PetrolIndexDatabase: RoomDatabase() {
         @Volatile
         private var INSTANCE: PetrolIndexDatabase? = null
 
+        /**
+         * Attribute stores the migration used to migrate the database from version 1 to version 2.
+         */
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE petrol_entries ADD COLUMN distanceTraveled INTEGER")
+            }
+        }
+
 
         /**
          * Method returns the singleton instance of the database.
@@ -42,7 +53,10 @@ abstract class PetrolIndexDatabase: RoomDatabase() {
                         context = context.applicationContext,
                         klass = PetrolIndexDatabase::class.java,
                         name = "petrol_index_database"
-                    ).fallbackToDestructiveMigration().build()
+                    )
+                        .fallbackToDestructiveMigration()
+                        .addMigrations(MIGRATION_1_2)
+                        .build()
                 }
                 return instance
             }
